9fd46f9d21dcebaa60108c22d9e4f6258e70da07
Prakhar Dwivedi <prakhar.dwivedi.7707@gmail.com>
2026-03-01 13:23:22 +0530

    Fix build crash and admin styling

--- config/settings.py
+++ config/settings.py
@@ -175,6 +175,8 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
